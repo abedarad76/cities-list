8d694ec435a7be1fb710a275ba04ac88c4ebd85a
abedarad76 <abedarad76@gmail.com>
2023-11-19 23:56:04 +0330

    handlSearch box

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let List = [];
+
 async function getCities() {
   try {
     const buttenText = document.getElementById("get-city-button-text");
@@ -7,35 +9,55 @@ async function getCities() {
       "https://countriesnow.space/api/v0.1/countries/population/cities"
     );
     const json = await response.json();
+    List = json.data;
+    console.log(json);
+    renderTabele(json.data);
 
     buttenText.innerHTML = "get Button plus Icon";
-    console.log(json.data);
-    
-
-    const table = document.getElementById("table-content")
-
-
-    json.data.slice(0,10).forEach((item) => {
-      const tableRow = document.createElement("tr");
-      tableRow.setAttribute("class", "mdc-data-table__row");
-
-      const cityCall = document.createElement("td");
-      cityCall.setAttribute("class","mdc-data-table__cell mdc-data-table__cell--numeric"
-      );
-      cityCall.innerHTML = item.city;
-      tableRow.appendChild(cityCall);
-
-      const countryCall = document.createElement("td");
-      countryCall.setAttribute(
-        "class",
-        "mdc-data-table__cell mdc-data-table__cell--numeric"
-      );
-      countryCall.innerHTML = item.country;
-      tableRow.appendChild(countryCall);
-
-      table.appendChild(tableRow)
-    });
   } catch (error) {
     console.log("amirrr", error);
   }
+}
+
+function renderTabele(lOND) {
+  const table = document.getElementById("table-content");
+  table.innerHTML = ""
+  lOND.forEach((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.setAttribute("class", "mdc-data-table__row");
+
+    const cityCall = document.createElement("td");
+    cityCall.setAttribute(
+      "class",
+      "mdc-data-table__cell mdc-data-table__cell--numeric"
+    );
+    cityCall.innerHTML = item.city;
+    tableRow.appendChild(cityCall);
+
+    const countryCall = document.createElement("td");
+    countryCall.setAttribute(
+      "class",
+      "mdc-data-table__cell mdc-data-table__cell--numeric"
+    );
+    countryCall.innerHTML = item.country;
+    tableRow.appendChild(countryCall);
+
+    table.appendChild(tableRow);
+  });
+}
+
+document.getElementById("search-input").addEventListener("input", handlSearch);
+
+function handlSearch(event) {
+  const searchvalue = event.target.value;
+  const filtring = List.filter((item) => {
+    const result = item.city.search(searchvalue);
+    if (result < 0) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  renderTabele(filtring);
 }
